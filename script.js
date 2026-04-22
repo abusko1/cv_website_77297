3cef68125e7ba65c4c81bc2fbea67a83670d9158
77297 Anna Busko <buskoanna525@gmail.com>
2026-04-22 18:30:28 +0300

    77297 zadania 5: dodanie formularza kontaktowego i walidacji w JS

--- script.js
+++ script.js
@@ -26,3 +26,54 @@ toggleBtn.addEventListener("click", function () {
         projekty.style.display = "none";
     }
 });
+
+
+const form = document.getElementById("contactForm");
+
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+const firstNameError = document.getElementById("firstNameError");
+const lastNameError = document.getElementById("lastNameError");
+const emailError = document.getElementById("emailError");
+const messageError = document.getElementById("messageError");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let isValid = true;
+
+    firstNameError.textContent = "";
+    lastNameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+
+    if (firstName.value.trim() === "" || /\d/.test(firstName.value)) {
+        firstNameError.textContent = "Niepoprawne imię";
+        isValid = false;
+    }
+
+    if (lastName.value.trim() === "" || /\d/.test(lastName.value)) {
+        lastNameError.textContent = "Niepoprawne nazwisko";
+        isValid = false;
+    }
+
+    // email
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email.value)) {
+        emailError.textContent = "Niepoprawny email";
+        isValid = false;
+    }
+
+    if (message.value.trim() === "") {
+        messageError.textContent = "Wiadomość jest wymagana";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Formularz wysłany poprawnie!");
+        form.reset();
+    }
+});
